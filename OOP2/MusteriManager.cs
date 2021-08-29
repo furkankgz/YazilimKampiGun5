ff363cb17e9b9795b51b0c29edf6bd16552e5318
Furkan <Furkan@DESKTOP-LC5065T>
2021-08-29 23:47:43 +0300

    Proje dosyası ekle.

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // İş Sınıfları - Business Classes
+    class MusteriManager
+    {
+        //Operasyon
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager IhtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager TasitKrediManager = new TasitKrediManager();
+            IKrediManager KonutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(KonutKrediManager, fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {IhtiyacKrediManager, TasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
